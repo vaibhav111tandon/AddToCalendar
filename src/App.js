e0fb6daf302820923e913826163a9aa48bee41aa
Vaibhav Tandon <vaibhav.tandongcet@gmail.com>
2021-04-29 18:57:24 +0530

    Added Preview button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { DatePicker, Divider, Input, Switch } from 'antd';
+import { DatePicker, Divider, Input, Switch, Button } from 'antd';
 
 import './App.css';
 import 'antd/dist/antd.css';
+
+import { CopyFilled, GithubOutlined } from '@ant-design/icons';
 
 import moment from "moment";
 
@@ -21,6 +23,7 @@ function App() {
     <div className="App">
       <header className="App__header">
         Add to Calendar
+        <p style={{fontWeight: 400, fontSize: '17px', color: '#565656', marginTop: '20px'}}>Generate Add to Calendar  links and event reminder buttons for Google Calendar</p>
         <Divider />
       </header>
       <section className='App__section'>
@@ -37,20 +40,29 @@ function App() {
           }
           <br/><br/>
           <TextArea
-          size="large"
-          placeholder="Event Description"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          autoSize={{ minRows: 3, maxRows: 50 }}
-        /><br/><br/>
-        <Input size="large" value={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`} /> <a href={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`}>Preview</a><br/>
-        <Input size="large" value={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`} /><br/>
-        <Input size="large" value={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`} /><br/>
-        <Input size="large" value={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`} /><br/>  
-
+            size="large"
+            placeholder="Event Description"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            autoSize={{ minRows: 3, maxRows: 50 }}
+          />
+          <br/><br/>
+          <Input 
+            size="large" 
+            value={`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`}
+            suffix={<CopyFilled style={{cursor: 'pointer'}} 
+            onClick={() => navigator.clipboard.writeText(`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`)}/>} 
+            />
+            <br/><br/>
+          <Button onClick={() => window.open(`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${moment.utc(date[0]).format('YYYYMMDDTHHmmss') + 'Z%2F' + moment.utc(date[1]).format('YYYYMMDDTHHmmss') + 'Z'}&details=${description}&location=${location}`, '_blank')}>
+            Preview
+          </Button>  
         </div>
-        
       </section>
+      <footer className="App__footer">
+        <Divider/>
+        Source Code: <a href="https://github.com/vaibhav111tandon" target="_blank" rel="noreferrer"> <GithubOutlined/></a>  
+      </footer>
     </div>
   );
 }
